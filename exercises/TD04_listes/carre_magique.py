e84a8d322b3e1c560355d5ea3c0bc4b5961e971c
Rim GNICHI <rim.gnichi@ens.uvsq.fr>
2022-02-11 10:48:39 +0100

    ras

---
+++ exercises/TD04_listes/carre_magique.py
@@ -0,0 +1,21 @@
+
+carre_mag = [[4,14,15,1] , [9,7,6,12], [5,11,10,8],[16,2,3,13]]
+carre_pas_mag = [[16,2,7,13]]
+
+def afficheCarre(carre):
+    """ Affiche la liste à 2 dimensions carre comme un carré"""
+    somme_ligne = somme_ligne + carre_mag(i)(j)
+    
+    def somme_ligne(carre_mag,i):
+        s = 0
+        for j in range(len(carre_mag)):
+           
+
+
+print(somme_ligne(carre_mag,1))
+
+
+
+afficheCarre(carre_mag)
+afficheCarre(carre_pas_mag)
+
